c3b5611cbb731640772101d4dc43e97cfcdcd5f5
veekshan <veekshanrai716@gmail.com>
2026-02-12 00:53:59 +0530

    Corrected folder inside another folder

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 var swiper = new Swiper(".mySwiper", {
+     loop: true,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
-        type: "progressbar",
+        clickable: true,
       },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+     
     });
